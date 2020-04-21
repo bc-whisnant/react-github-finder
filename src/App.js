ae8c7fed7ae99d82769f0155f88e6cc34841490a
brandondev <brandondev@brandondevs-Mac-mini.local>
2020-04-21 16:25:49 -0400

    added loading feature, refactored a few components to be functional instead of class based

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import React, {Component} from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
 
 // https://api.github.com/users
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    // change loading state
+    this.setState({
+      loading: true
+    })
+
+    const response = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+    this.setState({
+      users: response.data,
+      loading: false
+    })
+  }
   render() {
+    // destructure from state
+    let { loading, users} = this.state
+
     return (
       <div className="App">
-        <Navbar  />
+        <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={loading} users={users}/>
         </div>
       </div>
     );
   }
-  
 }
 
 export default App;
